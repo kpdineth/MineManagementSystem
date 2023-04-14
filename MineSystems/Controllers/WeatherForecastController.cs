4b77c51b1f54acb0caad52569030099c670617e6
Dineth Ponnamperuma <Dineth.Ponnamperuma@scopesystems.com.au>
2023-04-14 10:38:32 +0800

    Update API file with API Post, and try catch, and add a model folder

--- MineSystems/Controllers/WeatherForecastController.cs
+++ MineSystems/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MineSystems.Models;
 
 namespace MineSystems.Controllers
 {
@@ -9,7 +10,7 @@ namespace MineSystems.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -29,5 +30,80 @@ namespace MineSystems.Controllers
             })
             .ToArray();
         }
+
+        [HttpPost(Name = "PostWeatherForecast")]
+        public IActionResult Post()
+        {
+            var data = GenerateWeatherForecasts();
+            return Ok(data);
+        }
+
+        private IEnumerable<WeatherForecast> GenerateWeatherForecasts()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+        [HttpPost("PostNumber")]
+        public IActionResult PostNumber([FromBody] int number)
+        {
+            _logger.LogInformation($"Received numeric value: {number}");
+
+            // Process the numeric value here (e.g., save to a database, etc.)
+
+            return Ok("Numeric value processed successfully.");
+        }
+
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        [Route("api/[controller]/[action]")]
+        //[HttpPost(Name = "PrePostPersonData")]
+        public IActionResult PrePostPersonData([FromBody] Person Terson)
+        {
+            try
+            {
+                if (Terson == null || string.IsNullOrEmpty(Terson.Name) || Terson.Age <= 0)
+                {
+                    return BadRequest("Invalid person data.");
+                }
+
+                _logger.LogInformation($"Received person data: Name = {Terson.Name}, Age = {Terson.Age}");
+
+                // Process the person data here (e.g., save to a database, etc.)
+
+                return Ok("Person data processed successfully.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error processing person data: {ex.Message}");
+                return StatusCode(500, "An error occurred while processing the request.");
+            }
+        }
+
+
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        [Route("api/[controller]/[action]")]
+        public IActionResult PostPersonData([FromBody] Person person)
+        {
+            if (person == null || string.IsNullOrEmpty(person.Name) || person.Age <= 0)
+            {
+                return BadRequest("Invalid person data.");
+            }
+
+            _logger.LogInformation($"Received person data: Name = {person.Name}, Age = {person.Age}");
+
+            // Process the person data here (e.g., save to a database, etc.)
+
+            return Ok("Person data processed successfully.");
+        }
+
     }
+
+
+
 }
--- MineSystems/Program.cs
+++ MineSystems/Program.cs
@@ -1,19 +1,28 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.AddConsole();
+
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MineSystems", Version = "v1" });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MineSystems v1"));
 }
 
 app.UseHttpsRedirection();
